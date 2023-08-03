7af76ded14b0607d1a9548b196a7130bdd4e35f9
Illya Romaniv <mippikzilla@gmail.com>
2023-08-03 21:02:19 +0300

    fix run error

--- script.py
+++ script.py
@@ -7,10 +7,9 @@ bot = Bot(botToken, parse_mode="HTML")
 dp = Dispatcher(bot, storage=storage)
 
 if __name__ == '__main__':
-    from handlers import dp, send_to_admin, send_picture, echo_message
+    from handlers import dp, send_to_admin, send_picture
 
     # register handlers
     dp.register_message_handler(send_picture, lambda msg: msg, text='q')
-    dp.register_message_handler(echo_message, lambda msg: msg)
 
     executor.start_polling(dp, on_startup=send_to_admin)
